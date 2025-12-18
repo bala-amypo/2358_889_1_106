75d310180b073bb621f4dab578becfad6b11d773
bala-amypo <bala@amypo.in>
2025-12-18 10:42:39 +0000

    User commit

--- src/main/java/com/example/demo/controller/ComplianceEvaluationController.java
+++ src/main/java/com/example/demo/controller/ComplianceEvaluationController.java
@@ -11,7 +11,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/thresholds")
 @Tag(name = "Thresholds Endpoints")
-public class ComplianceThresholdController {
+public class ComplianceEvalutionController {
 
     private final ComplianceThresholdService thresholdService;
 
