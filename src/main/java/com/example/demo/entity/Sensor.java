0535b84a095239d8660875ce66d85f88c264695c
bala-amypo <bala@amypo.in>
2025-12-18 08:13:43 +0000

    User commit

--- src/main/java/com/example/demo/entity/Sensor.java
+++ src/main/java/com/example/demo/entity/Sensor.java
@@ -1,5 +1,4 @@
 package com.example.demo.entity;
-
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import java.time.LocalDateTime;
@@ -23,17 +22,13 @@ public class Sensor {
     @NotBlank(message = "sensorType is required")
     @Column(name = "sensor_type", nullable = false)
     private String sensorType;
-
     @ManyToOne
     @JoinColumn(name = "location_id", nullable = false)
     private Location location;
-
     @Column(name = "installed_at")
     private LocalDateTime installedAt;
-
     @Column(name = "is_active")
     private Boolean isActive;
-
     @PrePersist
     public void prePersist() {
         if (isActive == null) {
@@ -46,43 +41,33 @@ public class Sensor {
     public Long getId() {
         return id;
     }
-
     public String getSensorCode() {
         return sensorCode;
     }
-
     public void setSensorCode(String sensorCode) {
         this.sensorCode = sensorCode;
     }
-
     public String getSensorType() {
         return sensorType;
     }
-
     public void setSensorType(String sensorType) {
         this.sensorType = sensorType;
     }
-
     public Location getLocation() {
         return location;
     }
-
     public void setLocation(Location location) {
         this.location = location;
     }
-
     public LocalDateTime getInstalledAt() {
         return installedAt;
     }
-
     public void setInstalledAt(LocalDateTime installedAt) {
         this.installedAt = installedAt;
     }
-
     public Boolean getIsActive() {
         return isActive;
     }
-
     public void setIsActive(Boolean active) {
         isActive = active;
     }
