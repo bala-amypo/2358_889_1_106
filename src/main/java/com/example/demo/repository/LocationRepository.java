bc3b126b81edcdb088ed173c354662638bb70a25
bala-amypo <bala@amypo.in>
2025-12-18 09:43:42 +0000

    User commit

--- src/main/java/com/example/demo/repository/LocationRepository.java
+++ src/main/java/com/example/demo/repository/LocationRepository.java
@@ -3,5 +3,9 @@ package com.example.demo.repository;
 import com.example.demo.entity.Location;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface LocationRepository extends JpaRepository<Location, Long> {
+
+    Optional<Location> findByLocationName(String locationName);
 }
--- src/main/java/com/example/demo/repository/SensorRepository.java
+++ src/main/java/com/example/demo/repository/SensorRepository.java
@@ -3,5 +3,12 @@ package com.example.demo.repository;
 import com.example.demo.entity.Sensor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface SensorRepository extends JpaRepository<Sensor, Long> {
+
+    Optional<Sensor> findBySensorCode(String sensorCode);
+
+    List<Sensor> findByLocation_Region(String region);
 }
