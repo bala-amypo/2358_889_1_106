b72e1bc9ac3d3d4eeb9c24aabba6c144d1bb53ab
bala-amypo <bala@amypo.in>
2025-12-18 09:23:42 +0000

    User commit

--- src/main/java/com/example/demo/service/SensorService.java
+++ src/main/java/com/example/demo/service/SensorService.java
@@ -1,6 +1,9 @@
 package com.example.demo.service;
 
+import com.example.demo.entity.Location;
 import com.example.demo.entity.Sensor;
+import com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.repository.LocationRepository;
 import com.example.demo.repository.SensorRepository;
 import org.springframework.stereotype.Service;
 
@@ -10,16 +13,28 @@ import java.util.List;
 public class SensorService {
 
     private final SensorRepository sensorRepository;
+    private final LocationRepository locationRepository;
 
-    public SensorService(SensorRepository sensorRepository) {
+    public SensorService(SensorRepository sensorRepository,
+                         LocationRepository locationRepository) {
         this.sensorRepository = sensorRepository;
+        this.locationRepository = locationRepository;
     }
 
-    public Sensor save(Sensor sensor) {
+    public Sensor createSensor(Long locationId, Sensor sensor) {
+        Location location = locationRepository.findById(locationId)
+                .orElseThrow(() -> new ResourceNotFoundException("Location not found"));
+
+        sensor.setLocation(location);
         return sensorRepository.save(sensor);
     }
 
-    public List<Sensor> getAll() {
+    public Sensor getSensor(Long id) {
+        return sensorRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Sensor not found"));
+    }
+
+    public List<Sensor> getAllSensors() {
         return sensorRepository.findAll();
     }
 }
