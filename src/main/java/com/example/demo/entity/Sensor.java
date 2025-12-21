f3954721c73e4b6facc6b89a1676a81904b65f93
bala-amypo <bala@amypo.in>
2025-12-21 17:28:37 +0000

    User commit

--- src/main/java/com/example/demo/entity/Sensor.java
+++ src/main/java/com/example/demo/entity/Sensor.java
@@ -1,29 +1,30 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.*;
-import java.time.LocalDateTime;
 
 @Entity
 public class Sensor {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String sensorId;
-    private String type;
     
-    @ManyToOne
-    private Location location;
-    private LocalDateTime createdAt;
-    private Boolean active;
+    private String type; 
 
     public Sensor() {}
-    public Sensor(Long id, String sensorId, String type, Location location, LocalDateTime createdAt, Boolean active) {
-        this.id = id; this.sensorId = sensorId; this.type = type; this.location = location; this.createdAt = createdAt; this.active = active;
+
+    public Long getId() {
+        return id;
     }
 
-    public void setLocation(Location location) { this.location = location; }
-    public Location getLocation() { return this.location; }
-    
-    public String getSensorType() { return this.type; }
-    public void setSensorType(String sensorType) { this.type = sensorType; }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 }
