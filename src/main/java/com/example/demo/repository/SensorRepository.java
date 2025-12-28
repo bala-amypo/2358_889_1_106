e76422d7dd67a73ecb40f52756ffa7a6ccc933d5
bala-amypo <bala@amypo.in>
2025-12-28 16:57:45 +0000

    User commit

--- src/main/java/com/example/demo/repository/SensorRepository.java
+++ src/main/java/com/example/demo/repository/SensorRepository.java
@@ -1,13 +1,13 @@
 package com.example.demo.repository;
 
 import com.example.demo.entity.Sensor;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import java.util.Optional;
 import java.util.List;
 
-@Repository
-public interface SensorRepository extends JpaRepository<Sensor, Long> {
+public interface SensorRepository {
+    Sensor save(Sensor sensor);
+    Optional<Sensor> findById(Long id);
+    List<Sensor> findAll();
     Optional<Sensor> findBySensorCode(String sensorCode);
     List<Sensor> findByLocation_Region(String region);
 }
--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -1,11 +1,12 @@
 package com.example.demo.repository;
 
 import com.example.demo.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import java.util.Optional;
+import java.util.List;
 
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository {
+    User save(User user);
+    Optional<User> findById(Long id);
+    List<User> findAll();
     Optional<User> findByEmail(String email);
 }
